a0e5cb1738654cbccfee56fb30db7661a2a66b30
@midhun <midhunmidhunvm@gmail.com>
2025-02-20 13:40:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 LOG_GROUP_ID = -1002193329336
 
 # Get this value from @ultron2_robot on Telegram by /id
-OWNER_ID = @Midhuuuuuvee
+OWNER_ID = @Noxxy4544
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
